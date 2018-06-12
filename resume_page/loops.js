94444935f32f6d771106320ddbb5fe36b1b9d3c9
mac <mac@macs-MacBook-Pro.local>
2018-06-12 16:49:12 -0600

    Commit 9 - Created loops.js file

---
+++ resume_page/loops.js
@@ -0,0 +1,28 @@
+for (i = 0; i < 100; i++){
+	console.log(i);
+}
+
+for (i = 100; i > 0; i--){
+	console.log(i);
+}
+
+for (i = 0; i < 100; i+=2){
+	console.log(i );
+}
+
+for (i = 100; i > 0; i-=2){
+	console.log(i);
+
+}
+
+for (i = 49; i < 73; i++){
+	console.log(i);
+}
+
+for (i = 81; i > 25; i--){
+	console.log(i);
+}
+
+for (i = 3; i < 90; i+=3){
+	console.log(i );
+}
